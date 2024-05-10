3dff1f8a38e2a04af51e632a74f171f8239e1c76
aidanchappelle <149534841+aidanchappelle@users.noreply.github.com>
2024-05-10 13:23:06 -0300

    Update event.js

--- event.js
+++ event.js
@@ -1,5 +1,1 @@
-window.addEventListener('click', function () {
-
-if(document.querySelector('#video').paused){document.querySelector('#video').pause();} else {}
-   document.querySelector('#video').play();
- });
+anchorRef.addEventListener("markerFound", (e)=>{document.querySelector('#video').play()}
