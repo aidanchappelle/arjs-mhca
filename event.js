05f9167768ad4d53f4071f0991cc1b3c18cb4d21
aidanchappelle <149534841+aidanchappelle@users.noreply.github.com>
2024-06-04 15:49:29 -0300

    testing a thing

--- event.js
+++ event.js
@@ -1,6 +1,6 @@
-document.querySelector("#marker1").addEventListener('markerFound', function () { 
+window.addEventListener('markerFound', function () { 
     {document.querySelector('#video1').play();
     })
-document.querySelector("#marker1").addEventListener('markerLost', function () { 
+window.addEventListener('markerLost', function () { 
     {document.querySelector('#video1').pause();
     })
