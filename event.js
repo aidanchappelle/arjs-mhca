ed29e91b40ab8946c9db59a20e0af14f70af5aa3
aidanchappelle <149534841+aidanchappelle@users.noreply.github.com>
2024-08-08 13:56:36 -0300

    wait, if then

--- event.js
+++ event.js
@@ -1,6 +1,8 @@
 window.addEventListener('markerFound', function () { 
+    setTimeout(waitasec, 1000)
+    if (markerFound == true) {
     {document.querySelector('#video').play();
- }});
+    }}});
 
 window.addEventListener('markerLost', function () { 
     {document.querySelector('#video').pause();
