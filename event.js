c57b5c7ea23b742d3c298310e94b9bd655a387e5
aidanchappelle <149534841+aidanchappelle@users.noreply.github.com>
2024-08-08 15:47:44 -0300

    window

--- event.js
+++ event.js
@@ -4,7 +4,7 @@ window.addEventListener('markerFound', (e)=>{
     document.querySelector('#video').play();
     });
 
-anchorRef.addEventListener('markerLost', (e)=>{ 
+window.addEventListener('markerLost', (e)=>{ 
     setTimeout(1000)
     console.log('markerLost')
     document.querySelector('#video').play();
