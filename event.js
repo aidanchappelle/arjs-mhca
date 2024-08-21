27b88af33225e60c4be1f5f4032ab4c6474be90a
aidanchappelle <149534841+aidanchappelle@users.noreply.github.com>
2024-08-21 14:32:23 -0300

    e.target

--- event.js
+++ event.js
@@ -1,5 +1,5 @@
 window.addEventListener('markerFound', (e)=>{ 
-    console.log(e)
+    console.log(e.target)
     
     // Prevent asynchronous errors caused by play/pause.
     // https://goo.gl/LdLk22
@@ -18,7 +18,7 @@ window.addEventListener('markerFound', (e)=>{
 });
 
 window.addEventListener('markerLost', (e)=>{ 
-    console.log(e)
+    console.log(e.target)
     
     // Prevent asynchronous errors caused by play/pause.
     // https://goo.gl/LdLk22
