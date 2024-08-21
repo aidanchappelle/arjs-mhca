65dfa2b363b054a87314314859c96d9113cecf6c
aidanchappelle <149534841+aidanchappelle@users.noreply.github.com>
2024-08-21 09:29:18 -0300

    Update event.js

--- event.js
+++ event.js
@@ -1,11 +1,42 @@
 window.addEventListener('markerFound', (e)=>{ 
     setTimeout(1000)
-    console.log(window.markerFound)
+    console.log()
     document.querySelector('#video').play();
-    });
+    
+    // Prevent asynchronous errors caused by play/pause.
+    // https://goo.gl/LdLk22
+    
+    // Show loading animation.
+      var playPromise = video.play();
+      if (playPromise !== undefined) {
+        playPromise.then(_ => {
+          // Automatic playback started!
+          // Show playing UI.
+        })
+        .catch(error => {
+          // Auto-play was prevented
+          // Show paused UI.
+        });
+      }
+});
 
 window.addEventListener('markerLost', (e)=>{ 
     setTimeout(1000)
-    console.log(window.markerLost)
-    document.querySelector('#video').pause();
-    });
+    console.log(e)
+    
+    // Prevent asynchronous errors caused by play/pause.
+    // https://goo.gl/LdLk22
+    
+    // Show loading animation.
+      var playPromise = video.play();
+      if (playPromise !== undefined) {
+        playPromise.then(_ => {
+          // Automatic playback started!
+          // Show playing UI.
+        })
+        .catch(error => {
+          // Auto-play was prevented
+          // Show paused UI.
+        });
+      }
+});
