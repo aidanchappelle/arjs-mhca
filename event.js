ea78857781ed05f1346f90976e395cdc6818af63
aidanchappelle <149534841+aidanchappelle@users.noreply.github.com>
2024-08-23 14:40:09 -0300

    "simplified" the code

--- event.js
+++ event.js
@@ -1,39 +1,11 @@
 window.addEventListener('markerFound', (e)=>{ 
     console.log("Found",e.target.id)
-
-    // Prevent asynchronous errors caused by play/pause.
-    // https://goo.gl/LdLk22
-    
-    // Show loading animation.
-      var playPromise = document.querySelector('#target-'+e.target.id).play();
-      if (playPromise !== undefined || playPromise !== null) {
-        playPromise.then(_ => {
-            document.querySelector('#target-'+e.target.id).play()
-            console.log("Played",'#target-'+e.target.id)
-        })
-        .catch(error => {
-          // Auto-play was prevented
-          // Show paused UI.
-        });
-      }
+    document.querySelector('#target-'+e.target.id).play()
+    console.log("Played",'#target-'+e.target.id)
 });
 
 window.addEventListener('markerLost', (e)=>{ 
     console.log("Lost",e.target.id)
-    
-    // Prevent asynchronous errors caused by play/pause.
-    // https://goo.gl/LdLk22
-    
-    // Show loading animation.
-      var playPromise = document.querySelector('#target-'+e.target.id).pause();
-      if (playPromise !== undefined || playPromise !== null) {
-        playPromise.then(_ => {
-            document.querySelector('#target-'+e.target.id).pause()
-            console.log("Paused",'#target-'+e.target.id)
-        })
-        .catch(error => {
-          // Auto-play was prevented
-          // Show paused UI.
-        });
-      }
+    document.querySelector('#target-'+e.target.id).pause()
+    console.log("Paused",'#target-'+e.target.id)
 });
